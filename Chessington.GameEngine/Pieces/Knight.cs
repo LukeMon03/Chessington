e9f32e20f9e2f16b440a3f8e1be831392d5d60f7
Luke Monaghan <L.Monaghan@ucas.ac.uk>
2021-09-23 16:27:45 +0100

    Knights are a shmoving

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,58 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            Square newSquare = (Square.At(currentPosition.Row + 2, currentPosition.Col + 1));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+            newSquare = (Square.At(currentPosition.Row - 2, currentPosition.Col - 1));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+
+            newSquare = (Square.At(currentPosition.Row + 1, currentPosition.Col + 2));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+            newSquare = (Square.At(currentPosition.Row - 1, currentPosition.Col - 2));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+
+            newSquare = (Square.At(currentPosition.Row - 1, currentPosition.Col + 2));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+            newSquare = (Square.At(currentPosition.Row + 1, currentPosition.Col - 2));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+
+            newSquare = (Square.At(currentPosition.Row - 2, currentPosition.Col + 1));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+            newSquare = (Square.At(currentPosition.Row + 2, currentPosition.Col - 1));
+            if (newSquare.Row < 8 && newSquare.Row >= 0 && newSquare.Col < 8 && newSquare.Col >= 0)
+            {
+                availableSquares.Add(newSquare);
+            }
+
+
+            return availableSquares;
+
+            
         }
     }
 }
